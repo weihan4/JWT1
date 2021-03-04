79c012df8d989f2cae9f3ea36f0075f8e5007849
weihan <weihan4king@gamil.com>
2021-03-04 10:01:09 +0800

    added a bunch of stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import react, {useState, useEffect} from 'react';
 
-function App() {
+const App = () => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const changeUsernameHandler = (e) => {
+    const username = e.target.value;
+    setUsername(username);
+  };
+
+  const changePasswordHandler = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+    
+
   return (
     <div className="App">
-    
+      <div>
+        <input type='text' name='username' onChange={changeUsernameHandler} />
+        <label>Username</label>
+      </div>
+      <div>
+        <input type ='text' name='password' onChange={changePasswordHandler} />
+        <label>Password</label>
+      </div>
+      <div>
+        <button name='login' onClick={handleLogin} >
+          Login
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
